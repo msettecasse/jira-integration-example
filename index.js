f4ccf01f8e4dfc2248b232da9d0a6051daa52ed5
Mike Settecasse <miguel.andres@enel.com>
2025-01-13 15:19:09 +0100

    Starting Express and Jest project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.status(200).send("Hello World!");
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+module.exports = app;
